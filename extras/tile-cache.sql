0ce0a5ec7ee8f84d8f2a4423b0ac63d44f9bcffb
Daven Quinn <dev@davenquinn.com>
2023-02-16 06:13:25 -0600

    Cache updates

--- extras/tile-cache.sql
+++ extras/tile-cache.sql
@@ -23,3 +23,50 @@ CREATE TABLE IF NOT EXISTS tile_cache.tile (
   -- Make sure tile is within TMS bounds
   CHECK (x >= 0 AND y >= 0 AND z >= 0 AND x < 2^z AND y < 2^z)
 );
+
+CREATE INDEX IF NOT EXISTS tile_cache_tile_last_used_idx ON tile_cache.tile (last_used);
+
+CREATE OR REPLACE VIEW tile_cache.tile_info AS
+SELECT 
+  x,
+  y,
+  z,
+  layers,
+  length(tile) tile_size,
+  profile,
+  tms,
+  created,
+  last_used
+FROM tile_cache.tile;
+
+CREATE OR REPLACE FUNCTION tile_cache.remove_excess_tiles(max_size bigint DEFAULT 100000) RETURNS void AS $$
+DECLARE
+  _current_size bigint;
+  _num_deleted integer;
+BEGIN
+  /** Delete the most stale tiles until fewer than max_size tiles remain. */
+  -- Get approximate size of cache
+  SELECT pg_total_relation_size('tile_cache.tile') INTO _current_size;
+  
+  -- Get approximate number of tiles in cache table (without full table scan)
+  SELECT reltuples::bigint AS estimate
+  FROM pg_class
+  WHERE oid = 'tile_cache.tile'::regclass
+  INTO _current_size;
+
+  -- Delete tiles until cache size is less than max_size
+  _num_deleted := _current_size - max_size;
+
+  IF _current_size > max_size THEN
+    DELETE FROM tile_cache.tile
+    WHERE last_used < (
+      SELECT last_used FROM tile_cache.tile
+      ORDER BY last_used ASC
+      LIMIT 1
+      OFFSET _num_deleted
+    );
+
+    RAISE NOTICE 'Deleted % tiles to reduce cache size', _num_deleted;
+  END IF;
+END;
+$$ LANGUAGE plpgsql VOLATILE;
