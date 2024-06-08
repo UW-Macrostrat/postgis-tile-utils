1ef18f99436eff1d833b6b0463946644f249763a
Daven Quinn <code@davenquinn.com>
2024-06-08 14:23:00 -0500

    Yet more changes to tile cache

--- extras/tile-cache.sql
+++ extras/tile-cache.sql
@@ -1,31 +1,35 @@
 CREATE SCHEMA tile_cache;
 
 CREATE TABLE IF NOT EXISTS tile_cache.profile (
-  name text NOT NULL PRIMARY KEY,
+  id serial PRIMARY KEY,
+  name text NOT NULL UNIQUE,
   format text NOT NULL,
   content_type text NOT NULL,
   minzoom integer,
   maxzoom integer
 );
 
-/* We need to add a TMS column to support non-mercator tiles */
 CREATE TABLE IF NOT EXISTS tile_cache.tile (
   x integer NOT NULL,
   y integer NOT NULL,
   z integer NOT NULL,
-  profile text NOT NULL REFERENCES tile_cache.profile(name),
-  -- Hash of the params
-  params bytea NOT NULL,
+  profile integer NOT NULL REFERENCES tile_cache.profile(id),
+  -- For speed, we reduce the hash to an integer, increasing the likelihood of collisions
+  -- but reducing the size of the index and efficiency of querying over it. This could be
+  -- revisited if hash collisions become a problem, but they will only be important in edge
+  -- cases where the same tile is requested with different parameters.
+  -- We could also just index the parameters themselves (right now it's just t_step for paleogeography).
+  args_hash integer NOT NULL,
+  created timestamp without time zone NOT NULL DEFAULT now(),
+  last_used timestamp without time zone NOT NULL DEFAULT now(),
   /* TODO: we could cache each layer separately and merge in the tile server */
   --layers text[] NOT NULL,
   tile bytea NOT NULL,
-  tms text NOT NULL REFERENCES tile_utils.tms_definition(name) DEFAULT tile_utils.default_tms(),
-  created timestamp without time zone NOT NULL DEFAULT now(),
-  last_used timestamp without time zone NOT NULL DEFAULT now(),
-  PRIMARY KEY (x, y, z, params, profile),
+  PRIMARY KEY (x, y, z, profile, args_hash),
   -- Make sure tile is within TMS bounds
   CHECK (x >= 0 AND y >= 0 AND z >= 0 AND x < 2^z AND y < 2^z)
 );
+/* We'll need to add a TMS column if we want to support non-mercator tiles */
 
 
 CREATE INDEX IF NOT EXISTS tile_cache_tile_last_used_idx ON tile_cache.tile (last_used);
@@ -36,9 +40,8 @@ SELECT
   y,
   z,
   profile,
-  params,
+  args_hash,
   length(tile) tile_size,
-  tms,
   created,
   last_used
 FROM tile_cache.tile;
